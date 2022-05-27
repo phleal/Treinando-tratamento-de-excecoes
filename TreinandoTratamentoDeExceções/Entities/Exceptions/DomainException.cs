0af07a9f32e4fb7d6413649597b001fb238e04ec
phleal <philliplealgs@gmail.com>
2022-05-27 18:13:18 -0300

    first commit

---
+++ TreinandoTratamentoDeExceções/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TreinandoTratamentoDeExceções.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
